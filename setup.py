8655abdbfc1bbfbd8f9f4cd5773a3de6cc9952f5
g.racic <g.racic@criteo.com>
2022-10-05 08:57:19 +0200

    Release 0.0.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     name='centernet_fork',
     version='0.0.2',
     install_requires=_REQUIREMENTS_TXT,
-    description="Fork of repository https://github.com/xingyizhou/CenterNet2",
+    description="Fork of https://github.com/xingyizhou/CenterNet2",
     url="https://github.com/nateagr/CenterNet2",
     include_package_data=True,
     packages=setuptools.find_packages()
